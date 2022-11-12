fe208b3b4279712e0e78fb6ff6172225649fac4f
Format_C_eft <Format_C_eft@hotmail.com>
2022-11-12 21:29:47 +0500

    add git reset command

--- internal/cmd/init.go
+++ internal/cmd/init.go
@@ -15,14 +15,15 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.Version = "v0.0.1-alpha1"
+	rootCmd.Version = "v0.0.1"
 
 	rootCmd.Flags().StringVarP(&flagDir, "dir", "d", "../", "Путь к каталогу с проектами")
 	rootCmd.Flags().StringVarP(&flagDefaultBranch, "branch", "b", "master", "Наименование ветки: master/main")
 	rootCmd.Flags().BoolVarP(&flagAll, "all", "a", false, "Выполнить все шаги checkout master, fetch, pull")
-	rootCmd.Flags().BoolVarP(&flagAll, "checkout", "c", false, "Выполнить checkout master")
-	rootCmd.Flags().BoolVarP(&flagAll, "fetch", "f", false, "Выполнить fetch")
-	rootCmd.Flags().BoolVarP(&flagAll, "pull", "p", false, "Выполнить pull")
+	rootCmd.Flags().BoolVarP(&flagCheckout, "checkout", "c", false, "Выполнить checkout master")
+	rootCmd.Flags().BoolVarP(&flagFetch, "fetch", "f", false, "Выполнить fetch")
+	rootCmd.Flags().BoolVarP(&flagPull, "pull", "p", false, "Выполнить pull")
+	rootCmd.Flags().BoolVarP(&flagResetHard, "reset-hard", "r", false, "При необходимости перед git checkout выполнить git reset --hard")
 
 	flagExecuteTimeout = rootCmd.Flags().DurationP("execute_timeout", "e", time.Second*30, "Максимальное время обработки одного каталога")
 }
